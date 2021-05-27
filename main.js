5aedb64b286c4071b065f5afcfdd9b2954f29dea
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2021-05-27 16:52:24 +0700

    fix: change cors error to Indonesian

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ const corsOptions = {
           if (allowedDomains.indexOf(origin) !== -1) {
             callback(null, true);
           } else {
-            callback(new Error('Not allowed by CORS'));
+            callback(new Error('Tidak diizinkan oleh CORS'));
           }
         },
 };
