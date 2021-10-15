b056ac426473de3d88f4b19f8faee784c006ee50
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2021-10-15 10:41:24 +0700

    refactor: delete useless agent

--- src/main/whatsapp/index.js
+++ src/main/whatsapp/index.js
@@ -32,8 +32,6 @@ const waClient = new Client({
     ],
   },
   session: sessionCfg,
-  userAgent:
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36',
   restartOnAuthFail: true,
   takeoverOnConflict: true,
   takeoverTimeoutMs: 1000,
