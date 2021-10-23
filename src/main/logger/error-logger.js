3dc9d7d3c351bd10b33d06744f1e17b747b955bc
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2021-10-23 12:39:24 +0700

    feat: migrate to mutex function

--- src/main/logger/error-logger.js
+++ src/main/logger/error-logger.js
@@ -1,17 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const { rootPath } = require('../system/index');
-const ERROR_FILE_PATH = path.resolve(rootPath + '/wacsa-error.log');
-const { dateTimeGeneratorServer } = require('../../utils/dateTimeGenerator');
+const { errorFileHandle } = require('../mutex/error-file')
 
 async function errorLogger(errMsg, win) {
   try {
     await new Promise((resolve, reject) => {
-      const log = 'Error At ' + dateTimeGeneratorServer() + ' => ' + errMsg + '\r\n \r\n';
-      fs.writeFile(ERROR_FILE_PATH, log, { flag: 'a' }, (err) => {
-        if (err) reject(err);
-        resolve();
-      });
+      errorFileHandle(resolve, reject, errMsg, 1);
     });
   } catch (error) {
     win.webContents.send('error', error);
