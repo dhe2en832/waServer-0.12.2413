84bfe2bcf1192ae888999444ffba4a8a64d1b6d2
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-03-21 14:11:26 +0700

    feat: add new tagVersion func

--- src/main/system/index.js
+++ src/main/system/index.js
@@ -5,8 +5,10 @@ const ini = require('ini');
 
 const rootPath = app.isPackaged === false ? app.getAppPath() : path.dirname(app.getPath('exe'));
 const config = ini.parse(fs.readFileSync(path.resolve(rootPath + '/wacsa.ini'), 'utf-8'));
+const versionTag = app.getVersion();
 
 module.exports = {
   rootPath,
   config,
+  versionTag,
 };
