29e226c0885fdc69d3bb09755b8d0406a06e3a64
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-03-25 11:04:00 +0700

    feat: add new import SVGs

--- src/renderer/preload.js
+++ src/renderer/preload.js
@@ -4,6 +4,8 @@ const customTitlebar = require('custom-electron-titlebar');
 const { contextBridge, ipcRenderer } = require('electron/renderer');
 const { home } = require('./pages/home');
 const { login } = require('./pages/login');
+const showSVG = path.join(__dirname, '../images/show.svg');
+const hideSVG = path.join(__dirname, '../images/hide.svg');
 
 contextBridge.exposeInMainWorld('WACSA_UI', {
   titleBarSys: () => {
@@ -24,6 +26,10 @@ contextBridge.exposeInMainWorld('WACSA_UI', {
     }
   },
   mainSys: () => {
+    const icons = {
+      showSVG,
+      hideSVG,
+    }
     const wrapperElm = document.querySelector('main');
     const alertOnlineStatus = () => {
       navigator.onLine
@@ -32,12 +38,12 @@ contextBridge.exposeInMainWorld('WACSA_UI', {
           'Anda sedang offline. Silahkan koneksikan internet anda dan buka ulang aplikasi ini..!'
         );
     };
-    ipcRenderer.on('dom-loaded', (event, { port, version}) => {
+    ipcRenderer.on('dom-loaded', (event, { port, version }) => {
       window.addEventListener('online', alertOnlineStatus());
       window.addEventListener('offline', alertOnlineStatus());
       const base_url = 'http://localhost:' + port;
       if (JSON.parse(localStorage.getItem('sessionID'))) home(ipcRenderer, wrapperElm, version);
-      else login(ipcRenderer, wrapperElm, base_url, version, home);
+      else login(ipcRenderer, wrapperElm, base_url, version, icons, home);
     });
   },
 });
