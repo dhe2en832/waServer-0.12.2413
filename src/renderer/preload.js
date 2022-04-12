1ac325c31c1fccf727d66769ec9a2e5f2c2fdea7
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-04-12 10:28:20 +0700

    feat: add updater to preload renderer

--- src/renderer/preload.js
+++ src/renderer/preload.js
@@ -4,8 +4,11 @@ const customTitlebar = require('custom-electron-titlebar');
 const { contextBridge, ipcRenderer } = require('electron/renderer');
 const { home } = require('./pages/home');
 const { login } = require('./pages/login');
+const { updaterCustom } = require('./components/updaterCustom');
 const showSVG = path.join(__dirname, '../images/show.svg');
 const hideSVG = path.join(__dirname, '../images/hide.svg');
+const updateSVG = path.join(__dirname, '../images/update.svg');
+const restartSVG = path.join(__dirname, '../images/restart.svg');
 
 contextBridge.exposeInMainWorld('WACSA_UI', {
   titleBarSys: () => {
@@ -42,6 +45,7 @@ contextBridge.exposeInMainWorld('WACSA_UI', {
       window.addEventListener('online', alertOnlineStatus());
       window.addEventListener('offline', alertOnlineStatus());
       const base_url = 'http://localhost:' + port;
+      updaterCustom(ipcRenderer, updateSVG, restartSVG);
       if (JSON.parse(localStorage.getItem('sessionID'))) home(ipcRenderer, wrapperElm, version);
       else login(ipcRenderer, wrapperElm, base_url, version, icons, home);
     });
